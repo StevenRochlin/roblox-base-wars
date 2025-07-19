a83a04b5a32ccb531b07d2b83281a5285d32d594
visionarytomcat <visionarytomcat@gmail.com>
2025-07-19 15:40:13 -0700

    Changed sounds

--- src/shared/SoundAssets.lua
+++ src/shared/SoundAssets.lua
@@ -1,8 +1,8 @@
 local SoundAssets = {
-    Gold = 12221967, -- coin pickup
-    BodyShot = 12222156, -- hitmarker sound
-    Headshot = 169445602, -- headshot ding
-    Kill = 138186576, -- kill confirm
+    Gold = 607662191, -- coin pickup
+    BodyShot = 9116483270, -- hitmarker sound
+    Headshot = 17724151829, -- headshot ding
+    Kill = 7227567562, -- kill confirm
 }
 
 return SoundAssets 
